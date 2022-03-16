19e822c5f2658476a61265cb15d1264d316bbcb0
Aledev3 <99260616+Aledev3@users.noreply.github.com>
2022-03-16 15:53:38 +0100

    Update ded.py

--- Covid/ded.py
+++ Covid/ded.py
@@ -4,7 +4,7 @@ from _pycovid.py import def
 @_pycovid
 async def dedpepol(ded):
    covid = await logging.info(ded, ded.message)
-   await _pycovid.send_message(ded, f"{covid}")
+   await _pycovid.Send_Message(ded.chat.id, f"{covid}")
 
 
 #Go on
