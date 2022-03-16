9fb10e9ce0b4775fc8b67fcb2b8da7e0949c6425
Aledev3 <99260616+Aledev3@users.noreply.github.com>
2022-03-16 15:51:40 +0100

    Update ded.py
    fix send_message

--- Covid/ded.py
+++ Covid/ded.py
@@ -4,7 +4,7 @@ from _pycovid.py import def
 @_pycovid
 async def dedpepol(ded):
    covid = await logging.info(ded, ded.message)
-   await send_message(ded, f"{covid}")
+   await _pycovid.send_message(ded, f"{covid}")
 
 
 #Go on
